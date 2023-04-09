566cebb348418f7b0e907869e619828a395ae244
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-09 19:12:54 +0900

    :sparkles: :: 영화 검색 결과 lazycolumn 추가

--- app/src/main/java/com/example/yourmovie/presentation/view/screen/SearchScreen.kt
+++ app/src/main/java/com/example/yourmovie/presentation/view/screen/SearchScreen.kt
@@ -3,12 +3,16 @@ package com.example.yourmovie.presentation.view.screen
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.yourmovie.presentation.component.card.MovieCard
 import com.example.yourmovie.presentation.component.search.MovieSearchView
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -25,11 +29,27 @@ fun SearchScreen() {
             var textValue by remember { mutableStateOf("") }
 
             Box(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(20.dp)
             ) {
-                MovieSearchView(value = textValue, hint = "검색어를 입력하세요.", onValueChange = {text -> textValue = text })
+                MovieSearchView(
+                    value = textValue,
+                    hint = "검색어를 입력하세요.",
+                    onValueChange = {text -> textValue = text }
+                )
             }
+
+            val list = listOf(1,2,3,4,5,6,7,8)
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                content = {
+                    items(list) { item ->
+                        MovieCard()
+                    }
+                },
+                modifier = Modifier.padding(start = 10.dp, end = 10.dp)
+            )
         }
     }
 }
