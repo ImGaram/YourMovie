ace50b9943cbd43c667c12707bdf52f9aced4d2a
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-16 18:21:38 +0900

    :recycle: :: 중복된 scaffold 제거하기

--- app/src/main/java/com/example/yourmovie/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/yourmovie/presentation/view/MainActivity.kt
@@ -5,16 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.example.yourmovie.presentation.component.navigation.BottomNavigationView
-import com.example.yourmovie.presentation.view.ui.theme.YourMovieTheme
+import com.example.yourmovie.presentation.component.navigation.YourMovieNavItem
 import com.example.yourmovie.presentation.viewmodel.SearchMovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,13 +24,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                bottomBar = {
-                    BottomNavigationView(searchMovieViewModel = searchMovieViewModel)
-                }
+            Column(
+                modifier = Modifier.fillMaxSize()
             ) {
-
+                Box(modifier = Modifier.weight(1f))
+                BottomNavigationView(searchMovieViewModel = searchMovieViewModel)
             }
         }
     }
