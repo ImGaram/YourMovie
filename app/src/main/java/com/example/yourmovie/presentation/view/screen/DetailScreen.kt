d204bdcea8cbcc24c7fe7535ba3b8a8eb6816580
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-16 18:25:09 +0900

    :lipstick: :: detail screen 생성하기

---
+++ app/src/main/java/com/example/yourmovie/presentation/view/screen/DetailScreen.kt
@@ -0,0 +1,19 @@
+package com.example.yourmovie.presentation.view.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.yourmovie.model.data.MovieItemData
+
+@Composable
+fun DetailScreen(
+    data: MovieItemData?
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+    }
+}
