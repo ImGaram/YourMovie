33b9fbc8dbed50cda861f2f71de2dad7b4ed5369
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-04-18 11:11:32 +0900

    :sparkles: :: 캐스팅 정보 view model 생성하기

---
+++ app/src/main/java/com/example/yourmovie/presentation/viewmodel/CastingViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.yourmovie.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.yourmovie.model.data.CastingResponseData
+import com.example.yourmovie.model.usecase.CastingUseCase
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class CastingViewModel @Inject constructor(
+    private val castingUseCase: CastingUseCase
+): ViewModel() {
+    private val _casting = MutableStateFlow<CastingResponseData?>(null)
+    val casting: StateFlow<CastingResponseData?> = _casting
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    fun getCasting(movieId: Int, apiKey: String) = viewModelScope.launch {
+        castingUseCase(movieId, apiKey).onStart {
+            _isLoading.value = true
+        }.onCompletion {
+            _isLoading.value = false
+        }.catch { err ->
+            Log.e("TAG", "getCasting: ${err.printStackTrace()}", err.cause)
+        }.collect {
+            _casting.value = it
+        }
+    }
+}
